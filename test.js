6d9f9d407290b801c858d4656807c28b69f6e53e
Alejandro Stacul <116040128+alestacul60@users.noreply.github.com>
2024-02-08 20:42:30 -0300

    primera carga

---
+++ test.js
@@ -0,0 +1,10 @@
+
+function imprimir() {
+
+    var nombre = document.getElementById('nombre').value;
+    var domicilio = document.getElementById('domicilio').value;
+
+    alert('Mi nombre es ' + nombre + ' y vivo en ' + domicilio);
+    console.log('Mi nombre es ' + nombre + ' y vivo en ' + domicilio);
+
+}
